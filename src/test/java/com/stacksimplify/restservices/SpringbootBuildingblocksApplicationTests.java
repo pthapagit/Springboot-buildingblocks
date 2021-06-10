d3f17c48e1baac64d7272c7b884e5e7ae7030f4f
pthapagit <prabesh.usa@gmail.com>
2021-06-10 09:47:28 -0500

    Second commit try

--- src/test/java/com/stacksimplify/restservices/SpringbootBuildingblocksApplicationTests.java
+++ src/test/java/com/stacksimplify/restservices/SpringbootBuildingblocksApplicationTests.java
@@ -4,8 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+//new change
 class SpringbootBuildingblocksApplicationTests {
-
 	@Test
 	void contextLoads() {
 	}
