d7062e2aa9791cec63b6ad04e87a66afe16f0ece
pthapagit <80276713+pthapagit@users.noreply.github.com>
2021-06-21 16:05:37 -0500

    New change

--- src/main/java/com/stacksimplify/restservices/controllers/UserController.java
+++ src/main/java/com/stacksimplify/restservices/controllers/UserController.java
@@ -1,10 +1,14 @@
 package com.stacksimplify.restservices.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,5 +34,28 @@ public class UserController {
 		return userService.createUser(user);
 	}
 	
-
+	@GetMapping ("/users/{id}")
+	public Optional<User> getUserById(@PathVariable Long id) {
+		return userService.getUserById(id);
+	}
+	
+	//updateUserById
+	@PutMapping("/users/{id}")
+	public User updateUserById (@PathVariable ("id") Long id, @RequestBody User user) {
+		return userService.updateUserById(id, user);
+	}
+	
+	//deleteUserById
+	@DeleteMapping("/users/{id}")
+	public void deleteUserById (@PathVariable Long id){
+		userService.deleteUserById(id);
+	}
+	
+	//find iser by Username
+	@GetMapping("/users/byusername/{username}")
+	public User findUserByUsername(@PathVariable String username) {
+		return userService.getUserByUsername(username);
+	}
 }
+
+
--- src/main/java/com/stacksimplify/restservices/services/UserService.java
+++ src/main/java/com/stacksimplify/restservices/services/UserService.java
@@ -1,8 +1,11 @@
 package com.stacksimplify.restservices.services;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.apache.catalina.realm.UserDatabaseRealm;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
 
 import com.stacksimplify.restservices.entities.User;
@@ -13,15 +16,37 @@ import com.stacksimplify.restservices.repositories.UserRepository;
 public class UserService {
 	@Autowired
 	private UserRepository userRepository;
-	
-	
-	
-	public List<User> getAllUsers(){
+
+	public List<User> getAllUsers() {
 		return userRepository.findAll();
 	}
-	
-	//create user method
+
+	// create user method
 	public User createUser(User user) {
 		return userRepository.save(user);
+	}
+
+	// getUserMethodByID
+	public Optional<User> getUserById(Long id) {
+		return userRepository.findById(id);
+	}
+
+	// updateUserById
+	public User updateUserById(Long id, User user) {
+		user.setId(id);
+		return userRepository.save(user);
+	}
+
+	// deleteUserById
+	public void deleteUserById(Long id) {
+		if (userRepository.findById(id).isPresent()) {
+			userRepository.deleteById(id);
+		}
+	
+	}
+	
+	//get User by Username
+	public User getUserByUsername(String username) {
+		return userRepository.findByUsername(username);
 	}
 }
